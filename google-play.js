2660d2aa19acb13137921cfdd9915fa7f7335e2b
Ranjeet Rajput <ranjeet.rajput@v2solutions.com>
2018-07-01 22:44:58 +0530

    Create google-play.js

---
+++ google-play.js
@@ -0,0 +1,67 @@
+module.exports = {
+  'Google Play Demo' : function (client) {
+	  
+		save_directory = 'screenshots';
+		
+		if (process.argv.length < 5) {
+		  console.log('No URL was specified');
+		  client.end();
+		}
+
+		var url = process.argv[4];
+	  
+		client
+		.url(url)
+		.waitForElementVisible('body', 1000);
+
+
+		client.useXpath()     // every selector now must be XPath
+		client.assert.visible("//a[text()='Sign in']")
+		client.click("//a[text()='Sign in']")
+		client.useCss();
+
+		
+		
+		client.pause(5000)
+		.setValue('input[type=email]', 'facebook21r')
+		client.useXpath()     // every selector now must be XPath
+		client.assert.visible("//span[text()='Next']")
+		client.click("//span[text()='Next']")
+		client.useCss();
+		
+		client.pause(5000)
+		.setValue('input[type=password]', '21071990r')
+		client.useXpath()     // every selector now must be XPath
+		client.assert.visible("//span[text()='Next']")
+		client.click("//span[text()='Next']")
+		client.useCss();
+		
+		client.pause(5000)
+		
+		client.assert.visible(".oocvOe button:first-child");
+		client.click(".oocvOe button:first-child")
+		client.resizeWindow(1920, 3000);
+		client.pause(10000);
+		client.saveScreenshot(save_directory + '/screenshot'+uuidv4()+'.png')
+		client.end();
+
+
+		/*.assert.title('Google')
+		.assert.visible('input[type=text]')
+		.setValue('input[type=text]', 'rembrandt van rijn')
+		.waitForElementVisible('input[name=btnK]', 10000)
+		.click('input[name=btnK]')
+		.pause(1000)
+		.assert.containsText('ol#rso li:first-child',
+		'Rembrandt - Wikipedia') */
+  }
+};
+
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
